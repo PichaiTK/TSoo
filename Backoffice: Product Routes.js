beb41534420198274ab6d7076d91c33e0d66b9d1
Pichai Takengrach <tootack__@outlook.com>
2025-12-15 19:34:11 +0700

    Create Backoffice: Product Routes.js

---
+++ Backoffice: Product Routes.js
@@ -0,0 +1,17 @@
+// server/controllers/productController.js
+const Product = require("../models/Product");
+
+exports.getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+exports.createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.json(product);
+};
+
+exports.updateProduct = async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(product);
+};
