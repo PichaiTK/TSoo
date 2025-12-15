573cf83128bf3f1544c288b784ea8a7a7d849226
Pichai Takengrach <tootack__@outlook.com>
2025-12-15 19:10:35 +0700

    Create User.js

---
+++ User Model + Auth (F: Login + JWT + Role)  models/User.js
@@ -0,0 +1,23 @@
+// server/models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+  role: { type: String, enum: ["user", "editor", "admin"], default: "user" }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = function (plain) {
+  return bcrypt.compare(plain, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
